984d284bc8457123bfd6af36bdc9ecb81842110d
Lawani-EJ <elyonjohn60@gmail.com>
2024-06-11 00:19:14 +0100

    Added a contact us form to the Home Component Created a form with fields for name, email, and message. Used React useState to manage form input values, Added functions that will allow handling changes to input fields and form submission.

---
+++ src/components/Home.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { useState } from 'react'
+const Home = () => {
+    const [formDetails, setFormDetails] = useState({
+        name: '',
+        email: '',
+        message: '',
+    })
+
+    const handlingChanges = (e) => {
+        const {name, value} = e.target;
+        setFormDetails({
+            ...formDetails,
+            [name]: value
+        })
+    }
+
+    const handlingSubmission =(e) =>{
+        e.preventDefault();
+        console.log(formDetails);
+    }
+  return (
+    <form onSubmit={handlingSubmission}>
+    <div>
+        <label>
+            Enter Your Name
+            <input type="text" name='name' value={formDetails.name} onChange={handlingChanges}/>
+        </label>
+    </div>
+
+    <div>
+    <label>
+        Enter Your email address
+        <input type="email" name='email' value={formDetails.email} onChange={handlingChanges} />
+    </label>
+    </div>
+
+    <div> 
+        <label>
+            Kindly Enter Your Message
+            <input name='message' value={formDetails.message} onChange={handlingChanges} />
+        </label>
+    </div>
+    <button type='submit'>Submit Form</button>
+    </form>
+  )
+}
+
+export default Home
